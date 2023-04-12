06a78f1c2412d3631a09f7232b6a4bbf3d5d32aa
Helder Martins Gomes <helder.gomes@serpro.gov.br>
2023-04-12 00:04:46 -0300

    bugfixes

--- components/calendar/Calendar.tsx
+++ components/calendar/Calendar.tsx
@@ -7,6 +7,7 @@ import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { Absence } from '@prisma/client'
 import { sanitizeTime } from '../../lib/time'
+import TimeGutterHeader from './TimeGutterHeader'
 
 moment.locale('pt-BR')
 const localizer = momentLocalizer(moment)
@@ -33,6 +34,7 @@ function getCalendarEventsByAbsences(absences: Absence[]) {
       const endTime = a.endTimeAt ? new Date(sanitizeTime(a.endTimeAt)) : null
 
       let start, end
+      let allDay = false
       if (startTime && endTime) {
         start = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), startTime.getHours(), startTime.getMinutes(), 0)
         end = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate(), endTime.getHours(), endTime.getMinutes(), 0)
@@ -40,16 +42,16 @@ function getCalendarEventsByAbsences(absences: Absence[]) {
       } else {
         start = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate())
         end = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate(), 23, 59, 59)
+        allDay = true
       }
-console.log(`start: ${start}`);
-console.log(`end: ${end}`);
 
       return {
         id: a.id,
         title: a.user.name + ": " + a.title,
         start,
         end,
-        resourceId: 1,
+        allDay,
+        resourceId: 1, 
       }
     })
   }
@@ -60,17 +62,25 @@ interface CalendarProps {
   absences: Absence[]
 }
 
+
+
 export default function Calendar({ absences }: CalendarProps) {
   const events = getCalendarEventsByAbsences(absences)
 
   return (
     <div style={styles.container}>
       <BigCalendar
+        formats={{
+          timeGutterFormat: "HH:mm"
+        }}
+        components={{
+          timeGutterHeader: TimeGutterHeader,
+        }}
         selectable
         localizer={localizer}
         events={events}
         defaultView={Views.WEEK}
-        views={[Views.DAY, Views.WEEK, Views.MONTH]}
+        views={[Views.DAY, Views.WEEK, Views.MONTH, Views.AGENDA]}
         defaultDate={new Date()}
         resources={resourceMap}
         resourceIdAccessor="resourceId"
